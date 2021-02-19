0137a390ee5adedf7f98d405a78fef4de75d64f0
Arthur Campbell <arfa.camble@gmail.com>
2021-02-19 15:49:22 +0000

    improved cards on index page, improved seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,11 @@
 require 'open-uri'
 require 'json'
 
-puts 'deleting ingredients'
-Ingredient.destroy_all
 puts 'deleting cocktails and doses'
 Cocktail.destroy_all
+
+puts 'deleting ingredients'
+Ingredient.destroy_all
 
 puts 'seeding ingredients'
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
@@ -20,8 +21,33 @@ JSON.parse(open(url).read)['drinks'].each do |ing|
 end
 
 puts 'seeding cocktails'
-Cocktail.create!(name: 'White Russian')
-Cocktail.create!(name: 'Old Fashioned')
-Cocktail.create!(name: 'Dirty Martini')
-Cocktail.create!(name: 'Long Island Ice Tea')
-Cocktail.create!(name: 'Pint of')
+white_russian = Cocktail.new(name: 'White Russian')
+wr_img = URI.open('https://focusmicrosites.s3.amazonaws.com/assets/uploads/post_marquee_5f19f76b2870d.jpg')
+white_russian.photo.attach(io: wr_img, filename: 'white_russian.jpg', content_type: 'image/png')
+white_russian.save
+
+martini = Cocktail.new(name: 'Martini')
+martini_img = URI.open('https://i.pinimg.com/originals/ee/65/7f/ee657f437fac97abfed5784e1fb19b27.jpg')
+martini.photo.attach(io: martini_img, filename: 'martini.jpg', content_type: 'image/png')
+martini.save
+
+french_75 = Cocktail.new(name: 'French 75')
+french_75_img = URI.open('https://i0.wp.com/boingboing.net/wp-content/uploads/2019/01/Casablanca-review.jpg?fit=792%2C456&ssl=1')
+french_75.photo.attach(io: french_75_img, filename: 'french_75.jpg', content_type: 'image/png')
+french_75.save
+
+champagne_cocktail = Cocktail.new(name: 'Champagne Cocktail')
+champagne_cocktail_img = URI.open('http://lucyvhayauthor.com/wp-content/uploads/2017/01/Leonardo-DiCaprio-The-Great-Gatsby-Movie-Wallpapers.jpg')
+champagne_cocktail.photo.attach(io: champagne_cocktail_img, filename: 'champagne_cocktail.jpg', content_type: 'image/png')
+champagne_cocktail.save
+
+suntory = Cocktail.new(name: 'Suntory Whiskey')
+suntory_img = URI.open('https://i.insider.com/520532a069beddc374000011?width=700&format=jpeg&auto=webp')
+suntory.photo.attach(io: suntory_img, filename: 'suntory.jpg', content_type: 'image/png')
+suntory.save
+
+# Cocktail.create!(name: 'Old Fashioned')
+# Cocktail.create!(name: 'Dirty Martini')
+# Cocktail.create!(name: 'Long Island Ice Tea')
+# Cocktail.create!(name: 'Pint of')
+
