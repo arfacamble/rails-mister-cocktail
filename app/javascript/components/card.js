b607ed20bae3188f6bc91fb7eea90160d9d79236
Arthur Campbell <arfa.camble@gmail.com>
2021-02-21 15:55:21 +0000

    improved the dose dropdown styling and switching caret when menu open and closed

--- app/javascript/components/card.js
+++ app/javascript/components/card.js
@@ -1,9 +1,13 @@
 const initDisplayDoseFormOnClick = () => {
   const formClicker = document.querySelector('.form-click');
   const form = document.querySelector('.dose-form');
+  const downArrow = document.querySelector('.fa-caret-down');
+  const upArrow = document.querySelector('.fa-caret-up');
   if (formClicker) {
     formClicker.addEventListener('click', () => {
       form.classList.toggle('visible');
+      downArrow.classList.toggle('visible');
+      upArrow.classList.toggle('visible');
     });
   }
 }
